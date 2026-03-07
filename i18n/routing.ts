21a5985a8f201e0e52ab049599c9b6b652cd16bb
H3llmy <helmytrisna86@gmail.com>
2026-03-07 18:18:30 +0800

    fix: locale enum setup

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,15 +1,16 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+import { Locale } from '@/types';
+
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'id'],
- 
+  locales: [Locale.EN, Locale.ID],
+
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: Locale.EN
 });
- 
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
